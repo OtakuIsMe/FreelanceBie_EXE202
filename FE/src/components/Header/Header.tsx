e209b0fe414a4e07e0a9a0aa59101e215b57ea96
HungHy1602 <leconghung160202@gmail.com>
2024-12-24 11:17:54 +0700

    Update Header and Foooter

--- FE/src/components/Header/Header.tsx
+++ FE/src/components/Header/Header.tsx
@@ -1,26 +1,40 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import { IoSearch } from "react-icons/io5"; // Search icon from react-icons
+import "./Header.css";
 
 const Header = () => {
-  return (
-    <div id='main_header'>
-        <div className='logo'>FreelanceBie</div>
-        <div className='navbar'>
-            <ul>
-                <li><a href="#">Explore</a></li>
-                <li><a href="#">Hire a Designer</a></li>
-                <li><a href="#">Find Jobs</a></li>
-            </ul>
-        </div>
-        <div className='search'>
-            <p>What are you looking for</p>
-        </div>
-        <div className='auth'>
-            <button className='login btn'>Sign in</button>
-            <button className='register btn'>Sign up</button>
-        </div>
-    </div>
-  )
-}
+	return (
+		<header id="main_header">
+			<div className="logo">FreelanceBie</div>
+			<nav className="navbar">
+				<ul>
+					<li>
+						<a href="#">Explore</a>
+					</li>
+					<li>
+						<a href="#">Hire a Designer</a>
+					</li>
+					<li>
+						<a href="#">Find Jobs</a>
+					</li>
+				</ul>
+			</nav>
+			<div className="search">
+				<div className="search-input-wrapper">
+					<input
+						type="text"
+						placeholder="What are you looking for?"
+						className="search-input"
+					/>
+                    <IoSearch size={20} className="search-icon" />
+				</div>
+			</div>
+			<div className="auth">
+				<button className="btn login">Sign in</button>
+				<button className="btn register">Sign up</button>
+			</div>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
