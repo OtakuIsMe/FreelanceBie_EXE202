9bd35fa0777db3efca8cea48a815dae099020c5f
OtakuIsMe <duynmse173649@fpt.edu.vn>
2025-04-14 06:55:03 +0700

    .

--- FE/src/pages/User/Home/Home.tsx
+++ FE/src/pages/User/Home/Home.tsx
@@ -1,14 +1,5 @@
 import './Home.css'
-import Animation from '../../../assets/Animation.jpg'
-import Branding from '../../../assets/Branding.png'
-import Illustration from '../../../assets/Illustration.jpg'
-import Mobile from '../../../assets/Mobile.png'
-import Print from '../../../assets/Print.jpg'
-import ProductDesign from '../../../assets/Product_Design.jpg'
-import Typography from '../../../assets/Typography.jpg'
-import WebDesign from '../../../assets/Web_Design.jpg'
 import Header from '../../../components/Header/Header.tsx'
-import Specialities from '../../../components/Cards/Specialities/Specialities.tsx'
 import Explore from '../../../components/Cards/Explore/Explore.tsx'
 import Footer from '../../../components/Footer/Footer.tsx'
 import { Link, useLocation } from "react-router-dom";
@@ -31,17 +22,6 @@ export default function Home() {
 	const [shots, setShots] = useState<ShotView[] | null>(null)
 	const location = useLocation();
 
-	const spec = [
-		{ type: 'Animation', image: Animation },
-		{ type: 'Branding', image: Branding },
-		{ type: 'Illustration', image: Illustration },
-		{ type: 'Mobile', image: Mobile },
-		{ type: 'Print', image: Print },
-		{ type: 'ProductDesign', image: ProductDesign },
-		{ type: 'Typography', image: Typography },
-		{ type: 'WebDesign', image: WebDesign },
-	]
-
 	useEffect(() => {
 		fetchShotRandom();
 	}, [])
@@ -63,14 +43,6 @@ export default function Home() {
 							Get inspired with other designers
 						</p>
 						<button className='start-btn'>Get Started</button>
-					</div>
-				</div>
-				<div className="specialties section">
-					<p className='title'>Specialties</p>
-					<div className="spec-container">
-						{spec.map((spec, index) =>
-							<Specialities key={index} type={spec.type} img={spec.image} />
-						)}
 					</div>
 				</div>
 				<div className="explore section">
